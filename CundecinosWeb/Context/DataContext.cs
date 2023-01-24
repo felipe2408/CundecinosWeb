e596ee1a2d7cd7ad9a0646f077889e2fa63e4522
Brian Felipe Granados Giron <brian.granados@cmconsulting.com.co>
2023-01-24 15:18:26 -0500

    Add project files.

---
+++ CundecinosWeb/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CundecinosWeb.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            //Database.Migrate();
+        }
+        public DataContext()
+        {
+        }
+
+    }
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
