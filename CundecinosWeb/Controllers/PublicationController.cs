76870145463cc8a8ed91854ce04cf4d225a7d025
Brian Felipe Granados Giron <brian.granados@cmconsulting.com.co>
2023-04-18 14:47:53 -0500

    Mejoras

---
+++ CundecinosWeb/Controllers/PublicationController.cs
@@ -0,0 +1,22 @@
+﻿using CundecinosWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace CundecinosWeb.Controllers
+{
+	public class PublicationController : Controller
+	{
+        private readonly DataContext _context;
+        public PublicationController(DataContext context)
+		{
+			_context = context;
+
+		}
+		public IActionResult Index()
+		{
+			var model = _context.People.Include( x => x.CollegeCareer).Where(x => x.UID == Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier))).FirstOrDefault();
+			return View(model);
+		}
+	}
+}
