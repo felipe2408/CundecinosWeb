022050c85359834683acde782056339c05b6d491
Brian Felipe Granados Giron <brian.granados@cmconsulting.com.co>
2023-04-18 11:16:14 -0500

    Add Save Person

--- CundecinosWeb/Controllers/UserController.cs
+++ CundecinosWeb/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using CundecinosWeb.Models;
+﻿using Azure.Storage.Blobs;
+using CundecinosWeb.Data;
+using CundecinosWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -6,6 +8,12 @@ namespace CundecinosWeb.Controllers
 {
     public class UserController : Controller
     {
+        private readonly string _connectionString = "DefaultEndpointsProtocol=https;AccountName=cunpublication;AccountKey=8vdy7cuwlVkUdYw25qEnDcJqZy3DbktxPxxcUaw7ZB6Sh7fypyykIoHjK8irHbtN2hvdfxL4zO8l+AStvbK57A==;EndpointSuffix=core.windows.net";
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult PersonalInformation()
         {
             return View();
@@ -30,8 +38,32 @@ namespace CundecinosWeb.Controllers
         }
         [HttpPost]
 
-        public IActionResult Register(Person person)
+        public async Task<IActionResult> Register(Person person,IFormFile archivo)
         {
+            if (archivo == null || archivo.Length == 0)
+            {
+                return BadRequest("No se ha enviado ningún archivo");
+            }
+
+            // Crea un cliente del Blob Storage
+            BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+
+            // Crea una referencia al contenedor donde se guardará la imagen
+            string containerName = "publication";
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Genera un nombre único para la imagen
+            string nombreArchivo = Guid.NewGuid().ToString() + Path.GetExtension(archivo.FileName);
+
+            // Crea un blob con el nombre generado y sube el archivo
+            BlobClient blobClient = containerClient.GetBlobClient(nombreArchivo);
+            await blobClient.UploadAsync(archivo.OpenReadStream());
+            
+            person.AvatarUrl = blobClient.Uri.ToString();
+            person.UID = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            person.IsActive = true;
+            _context.Add(person);
+            _context.SaveChanges();
 
             return View();
         }
