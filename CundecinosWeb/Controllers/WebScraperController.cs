7a18cc4057b3efbf3ceb4484f436f86b9104fff2
Brian Felipe Granados Giron <brian.granados@cmconsulting.com.co>
2023-04-23 22:54:52 -0500

    Add Web Scraping

---
+++ CundecinosWeb/Controllers/WebScraperController.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CundecinosWeb.Controllers
+{
+    public class WebScraperController : Controller
+    {
+
+        [HttpPost]
+        public async Task<string?> ScrapePrice(string description)
+        {
+            var url = $"https://listado.mercadolibre.com.co/{description.Trim()}";
+
+            using var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            var firstResultLink = doc.DocumentNode.SelectSingleNode("//span[@class='price-tag-fraction']");
+            if (firstResultLink == null)
+            {
+                return null;
+            }
+            
+            var titleNode = doc.DocumentNode.SelectSingleNode("//h2[@class='ui-search-item__title shops__item-title']");
+
+            var price = $"El precio estimado para el producto {titleNode.InnerHtml} es :"+firstResultLink.InnerHtml;
+
+
+            return price;
+        }
+    }
+}
